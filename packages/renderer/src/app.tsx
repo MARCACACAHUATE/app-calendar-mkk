b9518fcd155d3b373a512c280fcdfe10f173a937
Gustavo Luna <marcacacahuate@outlook.com>
2022-11-16 00:13:12 -0600

    proyecto base cambiando vue por react

---
+++ packages/renderer/src/app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function App(){
+
+    return (
+        <div>
+            <p>Texto de super prueba</p>
+            <p>cena la cocena en la ofecena</p>
+        </div>
+    );
+}
+
+
+export default App;
