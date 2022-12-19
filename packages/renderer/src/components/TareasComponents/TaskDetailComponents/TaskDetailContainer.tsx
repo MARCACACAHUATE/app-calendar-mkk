2bc2ca9904e3c254853ad18be59356b190be84b4
MARCACACAHUATE <marcacacahuate@outlook.com>
2022-12-18 22:32:23 -0600

    renderer: TaskDetailComponents -> las personas asignadas ya se muestran en los detalles de las tareas

--- packages/renderer/src/components/TareasComponents/TaskDetailComponents/TaskDetailContainer.tsx
+++ packages/renderer/src/components/TareasComponents/TaskDetailComponents/TaskDetailContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams, Link } from 'react-router-dom';
 import { ModalContext } from '../../../context/ModalContext';
 import { Tareas } from '#preload';
@@ -10,6 +10,7 @@ interface Props {
 
 function TaskDetailContainer({tarea_data}: Props){
     const modalInfo = useContext(ModalContext);
+    const [dropList, setDropList] = useState<boolean>(false);
 
     // obtenemos el index de la tarea para extraerla del array
     const { index } = useParams();
@@ -21,6 +22,11 @@ function TaskDetailContainer({tarea_data}: Props){
     const dia_vencimiento = new Date(data.fecha_vencimiento).getDate();
     const dias_restantes = dia_vencimiento - dia_actual;
 
+    // seteamos los items que se mostraran en la lista de asignados
+    const lista_personas = dropList ? data.personas : data.personas.slice(0, 4);
+    const onDropListChange = () => {
+        setDropList(!dropList);
+    }
 
     return (
         <div className="flex flex-row text-sm">
@@ -28,7 +34,23 @@ function TaskDetailContainer({tarea_data}: Props){
                 <h1 className="text-xl">{ data.titulo }</h1>
                 <p><span className="font-bold">Fecha de Inicio:</span> { data.fecha_inicio.slice(0, 10) }</p>
                 <p><span className="font-bold">Fecha de Vencimiento:</span> { data.fecha_vencimiento.slice(0, 10) }</p>
-                <p><span className="font-bold">Asignado a:</span> { }</p>
+                <p><span className="font-bold">Asignado a:</span></p>
+                <ul>
+                { 
+                    lista_personas.map((persona, index) => (
+                        <li key={index}>- {persona.email}</li>
+                    ))
+                }
+                </ul>
+
+                { !dropList && (
+                <p className="text-[10px] -mb-2 text-center w-full">
+                    {data.personas.length - lista_personas.length} personas restantes
+                </p>)
+                }
+                <button onClick={onDropListChange} className="text-lg w-full">
+                    {dropList ? "" : ""}
+                </button>
                 <p><span className="font-bold">Dias Restantes:</span> { dias_restantes } dias</p>
                 <p><span className="font-bold">Nivel de Prioridad:</span> { data.prioridad }</p>
                 <p><span className="font-bold">Estado de la Tarea:</span> { data.estado }</p>
